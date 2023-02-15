4298a748b4903c2ac21ae28e346c3d07507834ae
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-02-16 00:41:38 +0530

    server.js file created

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const dotenv = require("dotenv");
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+// Declaring PORT
+const PORT = process.env.PORT || 8080;
+
+// Headers Cross Origin Resource Sharing
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
+
+// Linkage with static files on frontend part
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
+app.get("*", (req, res) => {
+    try {
+        res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));    
+    } 
+    catch (error) {
+        res.send("Stackoverflow is not accessible right now! Check back soon");
+    }
+});
+app.use("/upload", express.static(path.join(__dirname, "/../uploads")));
+
+// Parsing
+app.use(bodyParser.json({
+    limit: "10mb"
+}));
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: "10mb"
+}));
+
+// Server startup
+app.listen(PORT, () => {
+    console.log(`Server listening at PORT ${PORT}`);
+});
